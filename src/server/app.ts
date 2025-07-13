c96316e8315c314e79eb13e4517928bca85d465b
Sakib Islam <your-email@example.com>
2025-07-13 21:36:52 +0600

    fixed the cors

--- src/server/app.ts
+++ src/server/app.ts
@@ -3,7 +3,12 @@ import { routerController } from "../controllers/book.controller";
 const app: Application = express()
 const cors = require("cors")
 app.use(express.json())
-app.use(cors())
+app.use(cors(
+    {
+        origin:"https://extraordinary-croquembouche-6c3c62.netlify.app/",
+         methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    }
+))
 
 app.use('/', routerController)
 
